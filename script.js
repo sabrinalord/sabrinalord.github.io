9f06a732ad71403d28ed06911cdd7530064212d0
sabrinalord <sabrinalord@msn.com>
2021-08-01 14:15:08 +0100

    feat(experience-timeline)
    added javascript show/hide radio options

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ const hamburger = document.querySelector('.hamburger')
 	})
 
 	
+//TIMELINE
+
+// get radios
+
+	function ShowHideExperience(){
+      document.getElementById('panel-1').style.display = document.getElementById('radio-1').checked ?
+		"block" : "none";
+		    document.getElementById('panel-2').style.display = document.getElementById('radio-2').checked ?
+		"block" : "none";
+		    document.getElementById('panel-3').style.display = document.getElementById('radio-3').checked ?
+		"block" : "none";
+	}
+
+
+	
+// get tab panels
+// listen for click 
+//when you click on year button, remove class of visible from current panel 
+// add class of visible to selected button's content 
+// Loop through each panel description, if it is checked add styling, then 
+
+
