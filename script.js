e85e056a4c94eec3c02ba793b1c4d71b1738b9c5
sabrinalord <sabrinalord@msn.com>
2021-08-04 17:17:29 +0100

    feat(design-section)carousel
    added carousel to design section - made responsive

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function ShowHideExperience(){
 	document.getElementById('panel-3').style.visibility = document.getElementById('radio-3').checked ? "visible": "hidden";
 	}
 
+
+// DESIGN CAROUSEL
+
+$(document).ready(function(){
+
+  $('.carousel-slider1').slick({
+  infinite: true,
+ prevArrow:"<img class='a-left control-c prev slick-prev' src='images/icons/left-chevron.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='images/icons/right-chevron.png'>"
+  });
+	
+	 
+
+});
+
