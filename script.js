92bf4457ceb6bc6b8952c5885d97755dc11912e0
sabrinalord <sabrinalord@msn.com>
2021-08-05 16:50:18 +0100

    style(carousel)
    updated carousel images

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ $(document).ready(function(){
     nextArrow:"<img class='a-right control-c next slick-next' src='images/icons/right-chevron.png'>"
   });
 	
+	  $('.carousel-slider2').slick({
+  infinite: true,
+ prevArrow:"<img class='a-left control-c prev slick-prev' src='images/icons/left-chevron.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='images/icons/right-chevron.png'>"
+  });
+	
+	
 	 
 
 });
