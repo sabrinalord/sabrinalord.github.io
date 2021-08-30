60eadea5f7e13f7e090f885b7ed08b042da08914
sabrinalord <sabrinalord@msn.com>
2021-08-30 17:28:08 +0100

    added moving picture on scroll

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ hamburger.addEventListener('click', event => {
     $( ".draggable" ).draggable();
   } );
 	
+
+//IMAGE MOVE ON SCROLL DOWN
+const headshotContainer = document.querySelector('.headshot-container')
+
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){ 
+   let currentScroll = window.pageYOffset || document.documentElement.scrollTop; 
+   if (currentScroll > lastScrollTop){
+      headshotContainer.style.transform = "translate3d(0px, 52px, 0px)"
+   } else {
+          headshotContainer.style.transform = "translate3d(0px, 0px, 0px)"
+   }
+   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+}, false);
+
 //TIMELINE
 
 function ShowHideExperience(){
