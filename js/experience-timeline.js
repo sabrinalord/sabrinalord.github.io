69993cbed55c9fa8dfd2f97cab3e9ba4bd9c6146
slord <slord@deloitte.co.uk>
2022-12-13 14:53:56 +0000

    update experience function

--- js/experience-timeline.js
+++ js/experience-timeline.js
@@ -1,7 +1,7 @@
 function ShowHideExperience(){
-    document.getElementById('panel-2016').style.visibility = document.getElementById('radio-2016').checked ? "visible": "hidden";
-	document.getElementById('panel-2018').style.visibility = document.getElementById('radio-2018').checked ? "visible": "hidden";
-	document.getElementById('panel-2019').style.visibility = document.getElementById('radio-2019').checked ? "visible": "hidden";
-	document.getElementById('panel-2021').style.visibility = document.getElementById('radio-2021').checked ? "visible": "hidden";
-	document.getElementById('panel-2022').style.visibility = document.getElementById('radio-2022').checked ? "visible": "hidden";
+	let yearsOfExperience = document.querySelectorAll('.radio-year').length
+
+		for (let i = 1; i <= yearsOfExperience; i++) {
+			document.getElementById(`panel${i}`).style.visibility = document.getElementById(`radio${i}`).checked ? "visible": "hidden";
+		}
 	}
